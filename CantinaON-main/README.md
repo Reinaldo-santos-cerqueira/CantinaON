be3dd308a68a11db1d86eea499a6ca77c7e17211
Jean Lucas <85479359+JeanLucasDev@users.noreply.github.com>
2021-09-18 10:16:33 -0300

    Add files via upload

---
+++ CantinaON-main/README.md
@@ -0,0 +1,2 @@
+# CantinaON
+ Projeto da faculdade para uma cantina de uma escola com controles dos país e adição de saldo
---
+++ CantinaON-main/js/script.js
@@ -0,0 +1,19 @@
+/*Para fazer o menu ganhar a cor*/
+window.addEventListener("scroll", function(e) {
+    var header  =   document.querySelector(".menu_desktop");
+    header.classList.toggle("sticky",window.scrollY > 0);
+})
+
+var menu_mobile = document.querySelector("header .menu_mobile .mobile");
+var open_menu  =   document.querySelector("#open_menu");
+var exit_menu   = document.querySelector("#exit_menu");
+console.log(open_menu);
+open_menu.addEventListener('click',function(){
+    menu_mobile.style.top = '0px';
+    menu_mobile.style.transition =  '1s';
+
+});
+exit_menu.addEventListener('click',function(){
+    menu_mobile.style.top = '-500px';
+    menu_mobile.style.transition =  '1s';
+});
